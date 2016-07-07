ecd737fc61c8c42442c1a3939695735be0772e70
Camille <camilleseguette@gmail.com>
2016-07-07 15:27:55 -0500

    Assignment 19 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -3,13 +3,12 @@ require_relative 'entry'
 class AddressBook
   attr_reader :entries
 
-  def initializes
+  def initialize
     @entries = []
   end
 
 
   def add_entry(name, phone_number, email)
-
     index = 0
     entries.each do |entry|
 
@@ -18,7 +17,18 @@ class AddressBook
       end
       index += 1
     end
-
     entries.insert(index, Entry.new(name, phone_number, email))
   end
-end 
+
+  def remove_entry(name, phone_number, email)
+    index = 0
+    entries.each do |entry|
+
+      if name < entry.name
+        entry.delete
+      end
+      index -= -1
+      entries.delete(index, Entry.new(name, phone_number, email))
+    end
+  end
+end
