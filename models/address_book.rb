bb65fb08f2e5940c478466ab861341f03a7782d9
Camille <camilleseguette@gmail.com>
2016-07-13 17:27:33 -0500

    Assignment 22 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -52,4 +52,16 @@ class AddressBook
 
     return nil
   end
+
+  def iterative_search(name)
+    entries.each_with_index do |entry, index|
+      if name == entry.name
+        return entry
+      else
+        index += 1
+      end
+    end
+    return nil
+  end
+
 end
